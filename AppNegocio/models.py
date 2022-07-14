bec39476f2377727fb2b0c258360aa3bf96b2e4c
Juaniman <manzonijuan@gmail.com>
2022-07-14 18:54:55 -0300

    Subida previa a envio

--- AppNegocio/models.py
+++ AppNegocio/models.py
@@ -6,7 +6,7 @@ class Clientes(models.Model):
     nombre_cli = models.CharField(max_length=50)
     tipo_cli = models.CharField(max_length=50)
     direccion_cli = models.CharField(max_length=50)
-    email_cli = models.EmailField
+    email_cli = models.EmailField()
     fecha_alta_cli= models.DateField()
     def __str__(self):
         return self.nombre_cli+" "+self.tipo_cli
@@ -14,7 +14,7 @@ class Clientes(models.Model):
 class Proveedores(models.Model):
     nombre_prov = models.CharField(max_length=50)
     direccion_prov = models.CharField(max_length=50)
-    email_prov = models.EmailField
+    email_prov = models.EmailField()
     rubro_prov = models.CharField(max_length=50)
     fecha_alta_prov= models.DateField()
     def __str__(self):
@@ -28,7 +28,8 @@ class Articulos(models.Model):
     costo_art= models.FloatField()
     precio_venta_art= models.FloatField()
     def __str__(self):
-        return self.codigo_sku_art+" "+self.nombre_art+" "+self.familia_art
+        return self.nombre_art+" SKU# ("+self.codigo_sku_art+")"
+
 
 
 
