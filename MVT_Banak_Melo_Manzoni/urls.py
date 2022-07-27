10b7767981bef88979e1c30fe3d57b6acd3eddae
Juaniman <manzonijuan@gmail.com>
2022-07-27 15:43:10 -0300

    corregi inicio

--- MVT_Banak_Melo_Manzoni/urls.py
+++ MVT_Banak_Melo_Manzoni/urls.py
@@ -20,6 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('',inicio,name='inicio'),
     path('admin/', admin.site.urls),
     path('AppNegocio/', include('AppNegocio.urls')),
 
